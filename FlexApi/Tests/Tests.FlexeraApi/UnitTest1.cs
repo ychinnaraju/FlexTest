7f0d6cdabe7f37a225845f5a04d7b33bbbcf1429
Yuvaraj Chinnaraju <ychinnaraju@flexera.com>
2018-08-27 17:12:15 +1000

    adding tests

---
+++ FlexApi/Tests/Tests.FlexeraApi/UnitTest1.cs
@@ -0,0 +1,19 @@
+using FlexApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Tests.FlexeraApi
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var controller = new ValuesController();
+			var returnData = controller.Get();
+
+			Assert.AreEqual(4, returnData.Count());
+		}
+	}
+}
