35c6e8c4ddceb152a4c5d5659a47ee3327668678
Yuvaraj Chinnaraju <ychinnaraju@flexera.com>
2018-09-04 13:42:23 +1000

    adding executables

---
+++ FlexApi/FlexApi/Controllers/CampsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexApi.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/Camps")]
+	public class CampsController : Controller
+	{
+		[HttpGet("")]
+		public IActionResult Get()
+		{
+			return Ok(new { Name = "Yuvi", Color = "Orange" });
+		}
+	}
+}
